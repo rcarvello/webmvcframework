56cb18ff89dd5fe810a2f043e45da34d9c7d8a4e
Rosario Carvello <rosario.carvello@gmail.com>
2016-12-22 01:37:36 +0100

    Add files via upload

---
+++ controllers/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace controllers;
+
+use framework\Controller;
+use framework\View;
+class Main extends Controller
+{
+    public function autorun($parameter=null)
+    {
+        $view = new View("main");
+        $this->setView($view);
+    }
+}
