e5577b78a08cb9bee8a6214569e4f08d0f89d300
Rosario Carvello <rosario.carvello@gmail.com>
2017-03-13 11:35:57 +0100

    Add files via upload

--- models/example01/Hello.php
+++ models/example01/Hello.php
@@ -7,15 +7,27 @@ class Hello extends Model
 {
 
     private $data= "Welcome";
+    private $elements;
 
     public function __construct()
     {
         parent::__construct();
+        $this->initElements();
+    }
+
+    private function initElements()
+    {
+        $this->elements = array("item a", "item b", "item c");
     }
 
     public function getData()
     {
         return $this->data;
+    }
+
+    public function getElements()
+    {
+        return $this->elements;
     }
 
 }
