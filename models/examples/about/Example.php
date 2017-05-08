81a37f258443a50b71cfc0740be0edbd14fb5035
Rosario Carvello <rosario.carvello@gmail.com>
2017-05-09 01:00:54 +0200

    Add files via upload

---
+++ models/examples/about/Example.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Class Example
+ *
+ * {ModelResponsability}
+ *
+ * @package models\examples\about
+ * @category Application Model
+ * @author  {AuthorName} - {AuthorEmail}
+*/
+namespace models\examples\about;
+
+use framework\Model;
+
+class Example extends Model
+{
+    /**
+    * Object constructor.
+    *
+    */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+    * Autorun method. Put your code here for running it after object creation.
+    * @param mixed|array|null $parameters Additional parameters to manage
+    *
+    */
+    protected function autorun($parameters = null)
+    {
+
+    }
+}
