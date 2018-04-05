7360f4890d10024d74af7040095e054464e87de6
Rosario Carvello <rosario.carvello@gmail.com>
2018-04-05 14:57:11 +0200

    Update Dispatcher.php

--- framework/Dispatcher.php
+++ framework/Dispatcher.php
@@ -33,7 +33,6 @@
  * @filesource framework/Bean.php
  * @author Rosario Carvello <rosario.carvello@gmail.com>
  * @version GIT:v1.1.0
- * @see framework/Record
  * @copyright (c) 2016 Rosario Carvello <rosario.carvello@gmail.com> - All rights reserved. See License.txt file
  * @license BSD Clause 3 License
  * @license https://opensource.org/licenses/BSD-3-Clause This software is distributed under BSD-3-Clause Public License
