ba15a29bff587c8333b2c66fb8eab5593871c8b1
Rosario Carvello <rosario.carvello@gmail.com>
2020-05-25 15:59:55 +0200

    Update RBAC.php

--- framework/RBAC.php
+++ framework/RBAC.php
@@ -8,7 +8,7 @@
  * @filesource framework/RBAC.php
  * @author Rosario Carvello <rosario.carvello@gmail.com>
  * @version GIT:v1.1.0
- * @copyright (c) 2016 Rosario Carvello <rosario.carvello@gmail.com> - All rights reserved. See License.txt file
+ * @copyright (c)  2016 Rosario Carvello <rosario.carvello@gmail.com> - All rights reserved. See License.txt file
  * @license BSD Clause 3 License
  * @license https://opensource.org/licenses/BSD-3-Clause This software is distributed under BSD-3-Clause Public License
  */
