2778d0591c8f964fbb12a0c39725a1d8f5bab05b
Rosario Carvello <rosario.carvello@gmail.com>
2022-05-11 09:41:06 +0200

    Update Dispatcher.php

--- framework/Dispatcher.php
+++ framework/Dispatcher.php
@@ -5,7 +5,7 @@
  * @package framework
  * @filesource framework/Dispatcher.php
  * @author Rosario Carvello <rosario.carvello@gmail.com>
- * @version GIT:v1.1.0
+ * @version GIT:v1.1.
  * @copyright (c) 2016 Rosario Carvello <rosario.carvello@gmail.com> - All rights reserved. See License.txt file
  * @license BSD Clause 3 License.
  * @license https://opensource.org/licenses/BSD-3-Clause This software is distributed under BSD-3-Clause Public License
