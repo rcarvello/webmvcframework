0158192bb05c4c6dda1fb143158deab9b0f534d7
Rosario Carvello <rosario.carvello@gmail.com>
2023-05-12 12:17:23 +0200

    Add a Captcha Component Demo View

---
+++ views/examples/cms/CaptchaComponent.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class CaptchaComponent
+ *
+ * {ViewResponsability}
+ *
+ * @package controllers\examples\cms
+ * @category Application View
+ * @author  {AuthorName} - {AuthorEmail}
+*/
+namespace views\examples\cms;
+
+use framework\View;
+
+class CaptchaComponent extends View
+{
+
+    /**
+    * Object constructor.
+    *
+    * @param string|null $tplName The html template containing the static design.
+    */
+    public function __construct($tplName = null)
+    {
+        if (empty($tplName))
+            $tplName = "/examples/cms/captcha_component";
+        parent::__construct($tplName);
+    }
+    
+}
