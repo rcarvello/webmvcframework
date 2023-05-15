0784d468a5831def6a7562d9863007cd6792184d
Rosario Carvello <rosario.carvello@gmail.com>
2023-05-15 16:59:54 +0200

    Upadate logic for starting framework using PHP built in web server

--- mvc_bootstrap.bat
+++ mvc_bootstrap.bat
@@ -1,15 +1,16 @@
 REM Bootstrap file of Web MVC Framework for Windows
 REM
-REM Assuming you have many PHP releases extracted into many different sub folders of current .bat file.
+REM Assuming you have many PHP releases extracted into many different sub folders of a root folder.
 REM For example:
 REM
-REM c:\php_apps\mvc_bootstrap.php
-REM c:\php_apps\php56     folder where are located PHP version 5.6 files
-REM c:\php_apps\php74     folder where are located PHP version 7.4 files
-REM c:\php_apps\php80     folder where are located PHP version 8.0 files
-REM c:\php_apps\php82     folder where are located PHP version 8.2 files
+REM C:\Users\Saro\Desktop\Applicazioni PHP Desktop\phpapp Is the root path of installed PHP distributions
+REM C:\Users\Saro\Desktop\Applicazioni PHP Desktop\phpapp\php56  folder where are located PHP version 5.6 files
+REM C:\Users\Saro\Desktop\Applicazioni PHP Desktop\phpapp\php74  folder where are located PHP version 7.4 files
+REM C:\Users\Saro\Desktop\Applicazioni PHP Desktop\phpapp\php80  folder where are located PHP version 8.0 files
+REM C:\Users\Saro\Desktop\Applicazioni PHP Desktop\phpapp\php82  folder where are located PHP version 8.2 files
 REM
-REM Note: On each PHP folder you need to configure php.ini according to framework requirements
+REM Note: Each PHP distribution folder starts with "php" + version
+REM Note: On each PHP distribution you need to configure php.ini according to framework requirements
 @echo off
 cls
 echo Bootstrapping of PHP WEB MVC Framework
@@ -18,14 +19,15 @@ REM ********************************
 REM Set configuration variables here
 REM ********************************
 REM
-REM Set PHP distributions PATH (for example C:\php_apps or %~dp0 for current .bat folder)
-set PHP_ROOT_PATH=%~dp0
+REM Set PHP ROOT distributions PATH
+set PHP_ROOT_PATH=C:\Users\Saro\Desktop\Applicazioni PHP Desktop\phpapp
 REM
 REM Set PHP version SUFFIX PATH -- CHANGE THIS FOR SETTING PHP VERSION
-set PHP_VERSION=56
+set PHP_VERSION=82
 REM
 REM Set WEB MVC Framework PATH CHANGE THIS FOR SETTING HTTP ROOT FOLDER
-set FRAMEWORK_PATH=D:\gitmvc
+REM %~dp0 is the folder of current .bat script
+set FRAMEWORK_PATH=%~dp0
 REM
 REM ********************************
 REM End configuration variables
