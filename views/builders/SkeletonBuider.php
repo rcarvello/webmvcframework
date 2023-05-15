5ee4628b99be40edc252aa78fa6edcf114911d82
Rosario Carvello <rosario.carvello@gmail.com>
2023-05-15 16:36:42 +0200

    Update deprecated create_function

--- views/builders/SkeletonBuider.php
+++ views/builders/SkeletonBuider.php
@@ -169,7 +169,15 @@ class SkeletonBuider extends View
         if( $pascalCase == true ) {
             $string[0] = strtoupper($string[0]);
         }
+
+        /* deprecated php7 removed php 8
         $func = @create_function('$c', 'return strtoupper($c[1]);');
+        */
+
+        $func = function($c) {
+            return strtoupper($c[1]);
+        };
+
         return preg_replace_callback('/_([a-z])/', $func, $string);
     }
 
