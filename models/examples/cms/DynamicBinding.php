323eae37b0aa995ad115f71a2446f8b8a071af63
Rosario Carvello <rosario.carvello@gmail.com>
2023-05-16 08:49:53 +0200

    Add DynamicBinding Example - Model

---
+++ models/examples/cms/DynamicBinding.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Class DynamicBinding
+ *
+ * {ModelResponsability}
+ *
+ * @package models\examples\cms
+ * @category Application Model
+ * @author  {AuthorName} - {AuthorEmail}
+*/
+namespace models\examples\cms;
+
+use framework\Model;
+
+class DynamicBinding extends Model
+{
+    /**
+    * Object constructor.
+    *
+    */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+    * Autorun method. Put your code here for running it after object creation.
+    * @param mixed|array|null $parameters Additional parameters to manage
+    *
+    */
+    protected function autorun($parameters = null)
+    {
+
+    }
+}
