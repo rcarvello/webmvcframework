05c80adb0c65afc78cdbf9c8f1f9d9350b3198a3
Rosario Carvello <rosario.carvello@gmail.com>
2025-10-22 11:41:12 +0200

    Add Test support

--- tests/framework/ViewTest.php
+++ tests/framework/ViewTest.php
@@ -4,21 +4,26 @@ use PHPUnit\Framework\TestCase;
 
 require_once __DIR__ . '/TestHelper.php';
 require_once RELATIVE_PATH . 'framework/View.php';
+require_once RELATIVE_PATH . 'framework/exceptions/MVCException.php';
+require_once RELATIVE_PATH . 'framework/exceptions/NotInitializedViewException.php';
+require_once RELATIVE_PATH . 'framework/exceptions/TemplateNotFoundException.php';
+require_once RELATIVE_PATH . 'framework/exceptions/VariableNotFoundException.php';
 
 class TestableView extends \framework\View
 {
     public function __construct($content)
     {
-        $this->tpl = $content;
-        $this->blocks = [];
-        $this->currentBlock = '';
+        parent::__construct();
+        $this->replaceTpl($content);
     }
+
 }
 
 final class ViewTest extends TestCase
 {
     public function testSetVarReplacesPlaceholderInTemplate(): void
     {
+        $template = 'Hello {NAME}';
         $view = new TestableView('Hello {NAME}');
         $view->setVar('NAME', 'World');
 
@@ -27,21 +32,10 @@ final class ViewTest extends TestCase
 
     public function testHideRemovesSpecificBlock(): void
     {
-        $template = '<!-- BEGIN content -->Visible<!-- END content -->';
+        $template = '<!-- BEGIN content --> Visible <!-- END content -->More Content';
         $view = new TestableView($template);
-
         $view->hide('content');
-
         $this->assertStringNotContainsString('Visible', $view->parse());
-    }
-
-    public function testSetVarTemplatePathInjectsSiteUrl(): void
-    {
-        $view = new TestableView('Path: {TEMPLATE_PATH}');
-
-        $view->setVarTemplatePath();
-
-        $this->assertSame('Path: ' . SITEURL . '/', $view->parse());
     }
 
     public function testReplaceTplOverridesContent(): void
@@ -51,4 +45,26 @@ final class ViewTest extends TestCase
 
         $this->assertSame('New Content', $view->parse());
     }
+
+    public function testOpenBlockThenParseThenCloseThenShow(): void
+    {
+
+        $users = array(
+            array('UserName' => 'Mark', 'UserEmail' => 'mark@email.com'),
+            array('UserName' => 'Elen', 'UserEmail' => 'elen@email.com'),
+            array('UserName' => 'John', 'UserEmail' => 'john@email.com')
+        );
+        $template = '<!-- BEGIN Users -->[{UserName} {UserEmail}]<!-- END Users -->';
+        $expected = '<!-- BEGIN Users -->[Mark mark@email.com][Elen elen@email.com][John john@email.com]<!-- END Users -->';
+        $view = new TestableView($template);
+        $view->openBlock("Users");
+        foreach ($users as $user) {
+            $view->setVar("UserName", $user["UserName"]);
+            $view->setVar("UserEmail", $user["UserEmail"]);
+            $view->parseCurrentBlock();
+        }
+        $view->setBlock();
+        $this->assertSame($expected, $view->parse());
+    }
+
 }
